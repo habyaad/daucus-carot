51ee37948631a21a7e7702b9a05d4d6234a9169e
habyaad <adeomotoshan@gmail.com>
2024-04-27 14:20:49 +0100

    refactor: remove DOB add phone number

--- src/parent/dto/create-parent.dto.ts
+++ src/parent/dto/create-parent.dto.ts
@@ -1,14 +1,13 @@
 import {
   IsEmail,
   IsNotEmpty,
+  IsPhoneNumber,
   IsString,
-  Matches,
+  IsStrongPassword,
+  Length,
   MaxLength,
   MinLength,
 } from 'class-validator';
-
-const passwordRegEx =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,20}$/;
 
 export class CreateParentDto {
   @IsString()
@@ -24,16 +23,14 @@ export class CreateParentDto {
   lastName: string;
 
   @IsNotEmpty()
-  @IsEmail(null, { message: 'Please provide valid Email.' })
+  @IsEmail()
   email: string;
-  
+
   @IsNotEmpty()
-  @Matches(passwordRegEx, {
-    message: `Password must contain Minimum 8 and maximum 20 characters, 
-      at least one uppercase letter, 
-      one lowercase letter, 
-      one number and 
-      one special character`,
-  })
+  @IsPhoneNumber("NG")
+  phoneNumber: string;
+
+  @IsNotEmpty()
+  @IsStrongPassword()
   password: string;
 }
