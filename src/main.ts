507118056d4caa49d70fb68b441380859573c2a0
habyaad <adeomotoshan@gmail.com>
2024-04-27 14:19:18 +0100

    feat: enable logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger("Bootstrap");
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT_NO || 3005
+  await app.listen(port);
+  logger.log(`Backend app listening on port: ${port}`)
 }
 bootstrap();
