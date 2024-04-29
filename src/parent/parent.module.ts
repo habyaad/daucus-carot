3acc40a96897f1752a7a128a5d466ee1019f4f0d
habyaad <adeomotoshan@gmail.com>
2024-04-29 02:16:54 +0100

    feat: export parent repository

--- src/parent/parent.module.ts
+++ src/parent/parent.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ParentService } from './parent.service';
 import { ParentController } from './parent.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Parent } from './entities/parent.entity';
+import { ParentRepository } from './parent.repository';
+import { JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ParentController],
-  providers: [ParentService],
-  exports:[ParentService],
-  imports:[TypeOrmModule.forFeature([Parent])]
+  providers: [ParentService, ParentRepository, JwtService],
+  imports: [forwardRef(() => AuthModule), TypeOrmModule.forFeature([Parent]), ],
+  exports: [ParentService, ParentRepository],
 })
 export class ParentModule {}
