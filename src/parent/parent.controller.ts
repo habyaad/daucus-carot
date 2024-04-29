7a9023bf307c4602c7b59d3b0de508ef3beb516e
habyaad <adeomotoshan@gmail.com>
2024-04-29 02:16:28 +0100

    test: test guards

--- src/parent/parent.controller.ts
+++ src/parent/parent.controller.ts
@@ -1,23 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { ParentService } from './parent.service';
-import { CreateParentDto } from './dto/create-parent.dto';
 import { UpdateParentDto } from './dto/update-parent.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/auth/decorators/role.decorator';
 
 @Controller('parent')
 export class ParentController {
   constructor(private readonly parentService: ParentService) {}
 
-  @Post()
-  create(@Body() createParentDto: CreateParentDto) {
-    return this.parentService.create(createParentDto);
+  @Get() 
+  @Roles('parent')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async findAll(@GetUser() parent) {
+    return await this.parentService.findAll();
   }
-
-  @Get()
-  findAll() {
-    return this.parentService.findAll();
-  }
-
-  @Get(':id')
+ 
+  @Get(':id') 
   findOne(@Param('id') id: string) {
     return this.parentService.findOne(+id);
   }
