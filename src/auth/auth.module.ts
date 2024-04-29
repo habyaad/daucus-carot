770ea83f6115df0304460380652f967b22f82e18
habyaad <adeomotoshan@gmail.com>
2024-04-29 02:04:11 +0100

    feat: register jwtmodule and passportmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ParentModule } from 'src/parent/parent.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [ParentModule]
+  imports: [
+    forwardRef(() => ParentModule),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      global: true,
+      secret: ''+process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: parseInt(process.env.JWT_EXPIRES_IN) || 3600 },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  exports: [PassportModule, JwtStrategy],
 })
-export class AuthModule {
-
-}
+export class AuthModule {}
