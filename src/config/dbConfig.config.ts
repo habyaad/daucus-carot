fc9a2ff485b85ac6c57a380b75cfd5df9f31185d
habyaad <adeomotoshan@gmail.com>
2024-04-29 02:09:18 +0100

    extract database configuration options

--- src/config/dbConfig.config.ts
+++ src/config/dbConfig.config.ts
@@ -10,7 +10,7 @@ export const dbConfigOption: TypeOrmModuleOptions = {
     port: parseInt(process.env.DATABASE_PORT_NO, 10),
     password: process.env.DATABASE_PASSWORD,
     username: process.env.DATABASE_USER,
-    entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+    entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
     database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: true,
