9ff5a6e244c2113b19726215634080c259c0f86e
habyaad <adeomotoshan@gmail.com>
2024-04-29 01:55:30 +0100

    feat: implement sign in in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,21 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateParentDto } from 'src/parent/dto/create-parent.dto';
 import { Parent } from 'src/parent/entities/parent.entity';
+import { LoginDto } from './dto/login-data.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
-    @Post('sign-in')
-    signIn(){
-        return 'sign in';
+    @Post('signin')
+    @HttpCode(HttpStatus.OK)
+    async signIn(@Body(ValidationPipe) loginDto: LoginDto){
+        return await this.authService.signIn(loginDto);
     }
-    @Post('sign-up')
+    @Post('signup')
     async signUp(@Body(ValidationPipe) createParentDto: CreateParentDto): Promise<Parent>{
-        return await this.authService.createParentAccount(createParentDto);
+        return await this.authService.createParent(createParentDto);
     }
 }
  
