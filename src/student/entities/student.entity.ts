c30ad00f74da3c14b5c9b8aab0ee63b629a749a4
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:07:20 +0100

    refactor: modify student entity

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,24 +1,21 @@
-import { IsEmail } from 'class-validator';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
-import { GenderType } from '../enums/gender.enum';
+import { User } from 'src/common/entities/user.entity';
+import { GenderType } from 'src/common/enums';
+import { Parent } from 'src/parent/entities/parent.entity';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
-export class Student {
-  @PrimaryGeneratedColumn('uuid')
-  id: string;
-  @Column()
-  firstName: string;
-  @Column()
-  lastName: string;
+@Entity()
+export class Student extends User{
+  
   @Column()
   dateOfBirth: string;
-
-  @IsEmail()
-  @Column()
-  email: string;
 
   @Column({ type: 'enum', enum: GenderType })
   gender: GenderType;
 
-  @Column()
-  password: string;
+  @Column({unique: true})
+  username: string;
+
+  @ManyToOne(()=>Parent, (parent)=>parent.children, {cascade: true})
+  @JoinColumn()
+  parent: Parent
 }
