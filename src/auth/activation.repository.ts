9940d46ca6b834eb3302812d8895181fb5a625b6
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:06:07 +0100

    feat: create activation repository

---
+++ src/auth/activation.repository.ts
@@ -0,0 +1,26 @@
+import { Repository } from 'typeorm';
+import {
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Activation } from './entities/activation.entity';
+import { StringUtils } from 'src/common/helpers/string.utils';
+
+@Injectable()
+export class ActivationRepository extends Repository<Activation> {
+  logger: Logger = new Logger();
+  constructor(
+    @InjectRepository(Activation)
+    private activationRepository: Repository<Activation>,
+  ) {
+    super(
+        activationRepository.target,
+        activationRepository.manager,
+        activationRepository.queryRunner,
+    );
+  }
+  generateNewActivationCode(): string{
+    return StringUtils.generateActivationCode();
+  }
+}
