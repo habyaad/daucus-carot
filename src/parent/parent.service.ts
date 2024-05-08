3c2989deaf675e42bd5e166ef7ba357f1a660fed
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:24:54 +0100

    feat: create function to register child

--- src/parent/parent.service.ts
+++ src/parent/parent.service.ts
@@ -5,17 +5,25 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Parent } from './entities/parent.entity';
 import { ParentRepository } from './parent.repository';
 import { LoginDto } from 'src/auth/dto/login-data.dto';
-import { JwtPayloadDto } from 'src/auth/dto/payload.dto';
+import { JwtPayloadDto } from 'src/auth/dto/jwt-payload.dto';
+import { StudentRepository } from 'src/student/student.repository';
+import { CreateStudentDto } from 'src/student/dto/create-student.dto';
+import { User } from 'src/common/entities/user.entity';
+import { Student } from 'src/student/entities/student.entity';
 
 @Injectable()
 export class ParentService {
   constructor(
-    @InjectRepository(ParentRepository)
     private readonly parentRepository: ParentRepository,
+    private readonly studentRepository: StudentRepository,
   ) {}
-  
-  async findAll() {
-    return await this.parentRepository.find();
+
+  async fetch(id: string): Promise<Parent> {
+    return await this.parentRepository.findOneBy({ id });
+  }
+
+  async registerChild(createStudentDto: CreateStudentDto, parent: Parent) {
+    return await this.studentRepository.createStudent(createStudentDto, parent);
   }
 
   findOne(id: number) {
