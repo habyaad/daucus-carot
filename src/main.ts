af467fbd181eaf5c499cc2be48b5d124c519a8f8
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:13:13 +0100

    feat: implement swagger auto documentation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  const port = process.env.PORT_NO || 3005;
+  const port = process.env.PORT_NO || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Daucus Carot')
+    .setDescription('The Daucus Carot API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(port);
   logger.log(`Backend app listening on port: ${port}`);
 }
