f91cb559b710ad3820da7a16bd3eb3714bb6e422
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:15:14 +0100

    refactor: modify login dto

--- src/auth/dto/login-data.dto.ts
+++ src/auth/dto/login-data.dto.ts
@@ -1,9 +1,22 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsAlphanumeric, IsEmail, IsEnum, IsNotEmpty, IsString, ValidateIf } from "class-validator";
+import { UserType } from "src/common/enums";
 
 export class LoginDto{
-    @IsEmail()
+    @IsEmail({},{message:"email must be a valid address"})
+    @IsNotEmpty()
+    @ValidateIf(o=>o.userType === UserType.Parent)
     email: string;
 
+    @IsAlphanumeric()
+    @IsNotEmpty()
+    @ValidateIf(o=>o.userType === UserType.Student)
+    username: string;
+
     @IsString()
+    @IsNotEmpty()
     password: string
+
+    @IsEnum(UserType)
+    @IsNotEmpty()
+    userType: UserType
 }
