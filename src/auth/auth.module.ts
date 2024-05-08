ffb1897c7091360b6c3dba9c98e09ea5e4230a89
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:15:45 +0100

    feat: implement methods for account activation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,19 +5,25 @@ import { ParentModule } from 'src/parent/parent.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { ActivationRepository } from './activation.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Activation } from './entities/activation.entity';
+import { StudentModule } from 'src/student/student.module';
 
 @Module({
   controllers: [AuthController],
   imports: [
     forwardRef(() => ParentModule),
+    StudentModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       global: true,
       secret: ''+process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: parseInt(process.env.JWT_EXPIRES_IN) || 3600 },
     }),
+    TypeOrmModule.forFeature([Activation])
   ],
-  providers: [AuthService, JwtStrategy],
-  exports: [PassportModule, JwtStrategy],
+  providers: [ActivationRepository, AuthService, JwtStrategy, ],
+  exports: [PassportModule, JwtStrategy, ActivationRepository],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import {
   BadRequestException,
+  ConflictException,
   Injectable,
   Logger,
   NotFoundException,
@@ -8,65 +9,146 @@ import {
 import { CreateParentDto } from 'src/parent/dto/create-parent.dto';
 import { Parent } from 'src/parent/entities/parent.entity';
 import { LoginDto } from './dto/login-data.dto';
-import { JwtPayloadDto } from './dto/payload.dto';
+import { JwtPayloadDto } from './dto/jwt-payload.dto';
 import { ParentRepository } from 'src/parent/parent.repository';
-import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
-import { PostgresErrorCode } from 'src/common/enums';
+import { PostgresErrorCode, UserType } from 'src/common/enums';
 import { User } from 'src/common/entities/user.entity';
+import { ActivationRepository } from './activation.repository';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+import { Activation } from './entities/activation.entity';
+import { ActivationDto } from './dto/activate.dto';
+import { StringUtils } from 'src/common/helpers/string.utils';
+import { StudentRepository } from 'src/student/student.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly parentRepository: ParentRepository,
+    private readonly activationRepository: ActivationRepository,
+    private readonly studentRepository: StudentRepository,
     private readonly jwtService: JwtService,
+    @InjectEntityManager() private readonly entityManager: EntityManager,
   ) {}
   logger: Logger = new Logger('Auth Module');
-  async createParent(createParentDto: CreateParentDto): Promise<Parent> {
+  async createParent(createParentDto: CreateParentDto) {
     const { firstName, lastName, email, password, phoneNumber, userType } =
       createParentDto;
+
+    const activation: Activation = new Activation();
+    activation.code = this.activationRepository.generateNewActivationCode();
+
     const parent: Parent = new Parent();
-    const salt: string = await bcrypt.genSalt();
     parent.firstName = firstName;
     parent.lastName = lastName;
     parent.email = email;
-    parent.password = await this.hashPassword(password);
+    parent.password = await StringUtils.hashPassword(password);
     parent.phoneNumber = phoneNumber;
     parent.userType = userType;
+    parent.activation = activation;
 
     try {
-      return await this.parentRepository.save(parent);
+      // await this.entityManager.transaction(async (entityManager) => {
+
+      //   // Optionally, you can perform more operations...
+      // });
+      await this.parentRepository.save(parent);
+
+      return {
+        message: 'account created successfully',
+        activationCode: activation.code,
+      };
     } catch (error) {
       this.logger.error(error, error.stack);
       if (error.code == PostgresErrorCode.UniqueColumnDuplicateErrorCode) {
-        throw new BadRequestException('Phone number/Email exists');
+        throw new ConflictException('Phone number/Email exists');
       }
       throw error;
     }
   }
 
   async signIn(loginDto: LoginDto) {
-    const { email, password } = loginDto;
+    const { email, password, userType, username } = loginDto;
 
-    const parent: Parent = await this.parentRepository.findOneBy({ email });
-    if (!parent || (await parent.validatePassword(password)) === false) {
-      throw new NotFoundException('Incorrect email/password');
+    const key: string = userType === UserType.Parent ? email : username;
+    const user: User = await this.fetchUser(loginDto);
+    if (user.isActivated() === false) {
+      throw new NotFoundException('Incorrect details');
     }
-    const payload: JwtPayloadDto = { email };
-    return { access_token: await this.jwtService.signAsync(payload), parent };
+    const payload: JwtPayloadDto = { key, userType };
+    user.activationId;
+    return { access_token: await this.jwtService.signAsync(payload), user };
   }
 
   async validateUser(payload: JwtPayloadDto): Promise<User> {
-    const { email } = payload;
+    const { key, userType } = payload;
+    let user: User;
     try {
-      const user = await this.parentRepository.findOneBy({ email });
+      if (userType === UserType.Parent) {
+        user = await this.parentRepository.findOneBy({ email: key });
+      } else {
+        user = await this.studentRepository.findOneBy({ username: key });
+      }
       return user;
     } catch (error) {
       throw new UnauthorizedException();
     }
   }
 
-  async hashPassword(password: string): Promise<string> {
-    return await bcrypt.hash(password, 10);
+  async fetchActivationDetails(loginDto: LoginDto): Promise<Activation> {
+    try {
+      const user: User = await this.fetchUser(loginDto);
+      if (user.activation.code === null) {
+        throw new BadRequestException('Account has been activated before');
+      }
+      user.activation.code =
+        this.activationRepository.generateNewActivationCode();
+      user.save();
+      return user.activation;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async fetchUser(loginDto: LoginDto): Promise<User> {
+    const { email, password, userType, username } = loginDto;
+
+    let user: User;
+    try {
+      if (userType === UserType.Parent) {
+        user = await this.parentRepository.findOneBy({ email });
+      } else {
+        user = await this.studentRepository.findOneBy({ username });
+      }
+      if (!user || (await user.validatePassword(password)) === false) {
+        throw new NotFoundException('Incorrect details');
+      }
+      return user;
+    } catch (err) {
+      this.logger.error(err);
+      throw err;
+    }
+  }
+
+  async activate(activationDto: ActivationDto): Promise<User> {
+    const { email, code, userType, username } = activationDto;
+    const key: string = userType === UserType.Parent ? email : username;
+
+    try {
+      const user: User = await this.validateUser({ key, userType });
+      if (user.activation.code === null) {
+        throw new BadRequestException('Account has been activated before');
+      }
+      if (user.activation.isValid(code)) {
+        user.activation.code = null;
+        user.save();
+        return user;
+      } else {
+        throw new BadRequestException('Activation code error');
+      }
+    } catch (err) {
+      throw err;
+    }
   }
 }
