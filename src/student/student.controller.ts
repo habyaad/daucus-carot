2f570b27f2d7393335fa02acd4fb6ff8560d4ad5
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:35:50 +0100

    feat: create get student endpoint

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,34 +1,46 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, UseGuards } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/role.guard';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { Student } from './entities/student.entity';
 
 @Controller('student')
+@ApiTags('Student')
+@ApiBearerAuth()
+
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
 
-  @Post()
-  create(@Body() createStudentDto: CreateStudentDto) {
-    return this.studentService.create(createStudentDto);
+  @Get() 
+  @Roles('parent')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async fetchParent(@GetUser() student): Promise<Student> {
+    console.log(student);
+    return await this.studentService.fetchUserById(student.id);
   }
 
-  @Get()
-  findAll() {
-    return this.studentService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.studentService.findAll();
+  // }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.studentService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.studentService.findOne(+id);
+  // }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
-    return this.studentService.update(+id, updateStudentDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
+  //   return this.studentService.update(+id, updateStudentDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.studentService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.studentService.remove(+id);
+  // }
 }
