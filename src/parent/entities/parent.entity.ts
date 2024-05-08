a27d26a345059caeee5e0080faad538c7ee270e5
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:20:50 +0100

    refactor: modify parent entity

--- src/parent/entities/parent.entity.ts
+++ src/parent/entities/parent.entity.ts
@@ -1,8 +1,16 @@
 
 import { User } from 'src/common/entities/user.entity';
-import { Entity } from 'typeorm';
+import { Student } from 'src/student/entities/student.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity()
 export class Parent extends User{
+    @Column({ unique: true })
+    email: string;
+  
+    @Column({ length: 11, unique: true })
+    phoneNumber: string;
 
+    @OneToMany(()=>Student, (student)=>student.parent)
+    children: Student[]
 }
