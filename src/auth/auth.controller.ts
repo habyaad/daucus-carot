07ccf0549c94579800eaf139560dc380c53fa49d
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:11:56 +0100

    feat: create endpoints for activation code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
 import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateParentDto } from 'src/parent/dto/create-parent.dto';
-import { Parent } from 'src/parent/entities/parent.entity';
 import { LoginDto } from './dto/login-data.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { Activation } from './entities/activation.entity';
+import { ActivationDto } from './dto/activate.dto';
+import { User } from 'src/common/entities/user.entity';
 
 @Controller('auth')
+@ApiTags('Auth')
+
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
@@ -14,8 +19,20 @@ export class AuthController {
         return await this.authService.signIn(loginDto);
     }
     @Post('signup')
-    async signUp(@Body(ValidationPipe) createParentDto: CreateParentDto): Promise<Parent>{
+    async signUp(@Body(ValidationPipe) createParentDto: CreateParentDto){
         return await this.authService.createParent(createParentDto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('code')
+    async fetchCode(@Body(ValidationPipe) loginDto: LoginDto): Promise<Activation>{
+        return await this.authService.fetchActivationDetails(loginDto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('activate')
+    async activate(@Body(ValidationPipe) activationDto: ActivationDto): Promise<User>{
+        return await this.authService.activate(activationDto);
     }
 }
  
