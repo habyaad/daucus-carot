3775a0bf82053b2f5e59582e086b46bcca920641
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:26:32 +0100

    feat: register student repository

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Student } from './entities/student.entity';
+import { StudentRepository } from './student.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [StudentController],
-  providers: [StudentService],
+  providers: [StudentService, StudentRepository],
+  imports: [TypeOrmModule.forFeature([Student]), forwardRef(() => AuthModule)],
+  exports: [StudentRepository],
 })
 export class StudentModule {}
