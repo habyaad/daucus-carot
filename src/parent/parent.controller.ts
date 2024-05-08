3e3aacc07e38ffb0f4ad189ac49e5cc9d57ad8e7
habyaad <adeomotoshan@gmail.com>
2024-05-08 11:19:34 +0100

    feat: remove unimplemented endpoints

--- src/parent/parent.controller.ts
+++ src/parent/parent.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, ValidationPipe } from '@nestjs/common';
 import { ParentService } from './parent.service';
 import { UpdateParentDto } from './dto/update-parent.dto';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { GetUser } from 'src/auth/decorators/get-user.decorator';
 import { RolesGuard } from 'src/auth/guards/role.guard';
 import { Roles } from 'src/auth/decorators/role.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CreateStudentDto } from 'src/student/dto/create-student.dto';
+import { Parent } from './entities/parent.entity';
+import { Student } from 'src/student/entities/student.entity';
 
 @Controller('parent')
+@ApiTags('Parent')
+@ApiBearerAuth()
 export class ParentController {
   constructor(private readonly parentService: ParentService) {}
 
   @Get() 
   @Roles('parent')
   @UseGuards(JwtAuthGuard, RolesGuard)
-  async findAll(@GetUser() parent) {
-    return await this.parentService.findAll();
+  async fetchParent(@GetUser() parent): Promise<Parent> {
+    console.log(parent);
+    return await this.parentService.fetch(parent.id);
   }
  
-  @Get(':id') 
-  findOne(@Param('id') id: string) {
-    return this.parentService.findOne(+id);
+  @Post('/register/child') 
+  @Roles('parent')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  async registerChild(@GetUser() parent, @Body(ValidationPipe) createStudentDto: CreateStudentDto){
+    return await this.parentService.registerChild(createStudentDto, parent);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateParentDto: UpdateParentDto) {
-    return this.parentService.update(+id, updateParentDto);
-  }
+  // @Get(':id') 
+  // findOne(@Param('id') id: string) {
+  //   return this.parentService.findOne(+id);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.parentService.remove(+id);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateParentDto: UpdateParentDto) {
+  //   return this.parentService.update(+id, updateParentDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.parentService.remove(+id);
+  // }
 }
