891e8420a500052c97a769ec7b7f6e593587de1d
habyaad <adeomotoshan@gmail.com>
2024-05-10 12:27:27 +0100

    refactor: setup for typorm and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentModule } from './student/student.module';
 import { ParentModule } from './parent/parent.module';
-import { ConfigModule } from '@nestjs/config';
-import { dbConfigOption } from './config/dbConfig.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(dbConfigOption),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
+    }),
     AuthModule,
     StudentModule,
     ParentModule,
+    TaskModule,
   ],
   controllers: [AppController],
   providers: [AppService],
