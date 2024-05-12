2c2754826148566c5c7385992e6a292e7dc24886
habyaad <adeomotoshan@gmail.com>
2024-05-12 09:33:22 +0100

    feat: add description for login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,38 +1,55 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateParentDto } from 'src/parent/dto/create-parent.dto';
 import { LoginDto } from './dto/login-data.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Activation } from './entities/activation.entity';
 import { ActivationDto } from './dto/activate.dto';
 import { User } from 'src/common/entities/user.entity';
+import { UserResponseDto } from 'src/common/dtos/user-response.dto';
 
 @Controller('auth')
 @ApiTags('Auth')
-
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('signin')
-    @HttpCode(HttpStatus.OK)
-    async signIn(@Body(ValidationPipe) loginDto: LoginDto){
-        return await this.authService.signIn(loginDto);
-    }
-    @Post('signup')
-    async signUp(@Body(ValidationPipe) createParentDto: CreateParentDto){
-        return await this.authService.createParent(createParentDto);
-    }
+  @ApiOperation({
+    summary: 'username for student, email for parents, do not use both',
+  })
+  @ApiResponse({
+    status: 200,
+    type: UserResponseDto,
+  })
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body(ValidationPipe) loginDto: LoginDto) {
+    return await this.authService.signIn(loginDto);
+  }
+  @Post('signup')
+  async signUp(@Body(ValidationPipe) createParentDto: CreateParentDto) {
+    return await this.authService.createParent(createParentDto);
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Post('code')
-    async fetchCode(@Body(ValidationPipe) loginDto: LoginDto): Promise<Activation>{
-        return await this.authService.fetchActivationDetails(loginDto);
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('code')
+  async fetchCode(
+    @Body(ValidationPipe) loginDto: LoginDto,
+  ): Promise<Activation> {
+    return await this.authService.fetchActivationDetails(loginDto);
+  }
 
-    @HttpCode(HttpStatus.OK)
-    @Post('activate')
-    async activate(@Body(ValidationPipe) activationDto: ActivationDto): Promise<User>{
-        return await this.authService.activate(activationDto);
-    }
+  @HttpCode(HttpStatus.OK)
+  @Post('activate')
+  async activate(
+    @Body(ValidationPipe) activationDto: ActivationDto,
+  ): Promise<User> {
+    return await this.authService.activate(activationDto);
+  }
 }
- 
