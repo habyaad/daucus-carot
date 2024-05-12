463221838e7e8cf6fc6b9c20ab0b9cc91be1c68c
habyaad <adeomotoshan@gmail.com>
2024-05-13 00:32:00 +0100

    feat: import task module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -5,11 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Student } from './entities/student.entity';
 import { StudentRepository } from './student.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { TaskModule } from 'src/task/task.module';
 
 @Module({
   controllers: [StudentController],
   providers: [StudentService, StudentRepository],
-  imports: [TypeOrmModule.forFeature([Student]), forwardRef(() => AuthModule)],
-  exports: [StudentRepository],
+  imports: [
+    TypeOrmModule.forFeature([Student]),
+    forwardRef(()=>TaskModule) ,
+  ],
+  exports: [StudentService],
 })
 export class StudentModule {}
