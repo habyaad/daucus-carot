6d60f07cfcb9d403a9faa8a309ef4c15ba8e8730
habyaad <adeomotoshan@gmail.com>
2024-05-13 00:22:59 +0100

    feat: create task dto

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTaskDto {}
+import { Transform } from "class-transformer";
+import { IsDate, IsNotEmpty, IsNumber, IsOptional, IsString, Length, MinDate } from "class-validator";
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3,20)
+  task: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(3,100)
+  description: string;
+
+  @IsNumber()
+  @IsOptional()
+  reward: number;
+
+  @IsNotEmpty()
+  @Transform( ({ value }) => new Date(value))
+  @IsDate()
+  @MinDate(new Date())
+  dueDate: string;
+
+}
