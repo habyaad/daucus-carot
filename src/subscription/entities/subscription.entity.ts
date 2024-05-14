40d30cd4f640b5ad8dbd521ac72124de94c7b0f3
habyaad <adeomotoshan@gmail.com>
2024-05-14 10:45:43 +0100

    feat: create subscription entity

--- src/subscription/entities/subscription.entity.ts
+++ src/subscription/entities/subscription.entity.ts
@@ -1,1 +1,31 @@
-export class Subscription {}
+import { DateColumn } from 'src/common/entities/date-column';
+import { SubscriptionPlan } from 'src/subscription-plan/entities/subscription-plan.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Subscription extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @OneToOne(() => SubscriptionPlan)
+  @JoinColumn()
+  plan: SubscriptionPlan;
+
+  @Column()
+  dueDate: Date;
+
+  @Column(() => DateColumn)
+  dates: DateColumn;
+
+  isActive(): boolean {
+    const now: Date = new Date();
+    return this.dueDate > now;
+  }
+}
