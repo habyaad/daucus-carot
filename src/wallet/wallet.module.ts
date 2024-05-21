2baf2b7097cc8496fca713e2e8c6c6b54bedb1af
habyaad <adeomotoshan@gmail.com>
2024-05-21 11:33:24 +0100

    feat: export wallet service

--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -3,10 +3,12 @@ import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Wallet } from './entities/wallet.entity';
+import { WalletRepository } from './wallet.repository';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Wallet])],
   controllers: [WalletController],
-  providers: [WalletService],
+  providers: [WalletService, WalletRepository],
+  exports:[WalletService]
 })
 export class WalletModule {}
