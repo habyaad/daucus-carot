ba2bcb7c2c97623a88d080ecf81c46f58ae9e44a
habyaad <adeomotoshan@gmail.com>
2024-05-21 11:33:53 +0100

    feat: link wallet to user

--- src/wallet/entities/wallet.entity.ts
+++ src/wallet/entities/wallet.entity.ts
@@ -1,14 +1,28 @@
 import { DateColumn } from 'src/common/entities/date-column';
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/common/entities/user.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Wallet extends BaseEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
-  @Column({default:0})
+
+  @Column({ default: 0 })
   balance: number;
-  @Column({default:0})
-  points: number;
+  
+  @OneToOne(() => User, (user) => user.wallet)
+  user: User;
+
+  // @Column()
+  // userId: string;
+
   @Column(() => DateColumn)
   dates: DateColumn;
 }
