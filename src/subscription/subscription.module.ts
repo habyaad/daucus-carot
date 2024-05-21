8c52c529656169c3db408db41ef64b109796723b
habyaad <adeomotoshan@gmail.com>
2024-05-21 11:41:21 +0100

    feat: register subscription repository

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -3,10 +3,14 @@ import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subscription } from './entities/subscription.entity';
+import { SubscriptionRepository } from './subscription.repository';
+import { SubscriptionPlanModule } from 'src/subscription-plan/subscription-plan.module';
+import { ParentModule } from 'src/parent/parent.module';
+import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Subscription])],
+  imports:[TypeOrmModule.forFeature([Subscription]), SubscriptionPlanModule, ParentModule, WalletModule],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService],
+  providers: [SubscriptionService, SubscriptionRepository],
 })
 export class SubscriptionModule {}
