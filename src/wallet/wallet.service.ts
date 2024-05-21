d8d2ef688aeaa3e62011d1a2b0e314fa94df621f
habyaad <adeomotoshan@gmail.com>
2024-05-21 11:33:12 +0100

    feat: implement method to credit and debit wallet

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Wallet } from './entities/wallet.entity';
+import { WalletRepository } from './wallet.repository';
 
 @Injectable()
 export class WalletService {
+  constructor(private readonly walletRepository: WalletRepository) {}
   create(createWalletDto: CreateWalletDto) {
     return 'This action adds a new wallet';
   }
@@ -15,6 +22,15 @@ export class WalletService {
   findOne(id: number) {
     return `This action returns a #${id} wallet`;
   }
+  // async findOneByUserID(id: string): Promise<Wallet> {
+  //   const wallet: Wallet = await this.walletRepository.findOneBy({
+  //     userId: id,
+  //   });
+  //   if (!wallet) {
+  //     throw new NotFoundException('Wallet not found for user');
+  //   }
+  //   return wallet;
+  // }
 
   update(id: number, updateWalletDto: UpdateWalletDto) {
     return `This action updates a #${id} wallet`;
@@ -22,5 +38,22 @@ export class WalletService {
 
   remove(id: number) {
     return `This action removes a #${id} wallet`;
+  }
+
+  async creditWallet(wallet:Wallet, amount: number): Promise<Wallet> {
+    //const wallet: Wallet = await this.findOneByUserID(userId);
+    wallet.balance += amount;
+    await wallet.save();
+    return wallet;
+  }
+  
+  async debitWallet(wallet:Wallet, amount: number): Promise<Wallet> {
+    //const wallet: Wallet = await this.findOneByUserID(userId);
+    if (amount > wallet.balance) {
+      throw new BadRequestException('Insufficient funds');
+    }
+    wallet.balance -= amount;
+    await wallet.save();
+    return wallet;
   }
 }
