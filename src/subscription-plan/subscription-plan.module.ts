01947734608289689565b4923f842426425859f2
habyaad <adeomotoshan@gmail.com>
2024-05-21 11:37:27 +0100

    feat: export subscription plan service

--- src/subscription-plan/subscription-plan.module.ts
+++ src/subscription-plan/subscription-plan.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionPlanService } from './subscription-plan.service';
 import { SubscriptionPlanController } from './subscription-plan.controller';
-import { SubscriptionPlanRepository } from './subscription.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubscriptionPlan } from './entities/subscription-plan.entity';
+import { SubscriptionPlanRepository } from './subscription-plan.repository';
 
 @Module({
   imports:[TypeOrmModule.forFeature([SubscriptionPlan])],
   controllers: [SubscriptionPlanController],
   providers: [SubscriptionPlanService, SubscriptionPlanRepository],
+  exports:[SubscriptionPlanService]
 })
 export class SubscriptionPlanModule {}
