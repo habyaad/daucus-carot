baada9d6a9f42f03bb308405451fdedd51222947
habyaad <adeomotoshan@gmail.com>
2024-05-21 12:55:34 +0100

    feat: register transaction repository

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -3,10 +3,11 @@ import { TransactionService } from './transaction.service';
 import { TransactionController } from './transaction.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
+import { TransactionRepository } from './transaction.repository';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Transaction])],
   controllers: [TransactionController],
-  providers: [TransactionService],
+  providers: [TransactionService, TransactionRepository],
 })
 export class TransactionModule {}
