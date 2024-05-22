31a094eb717c4abfe98d759ebad6972ff6b3fa03
habyaad <adeomotoshan@gmail.com>
2024-05-22 13:09:23 +0100

    feat: create transaction dto

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTransactionDto {}
+import { User } from 'src/common/entities/user.entity';
+import { TaskStatus, TransactionCategory } from 'src/common/enums';
+
+export class CreateTransactionDto {
+  reference: string;
+  amount: number;
+  category: TransactionCategory;
+  reason?: string;
+  narration: string;
+  user: User;
+}
