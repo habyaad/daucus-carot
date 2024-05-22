8432cbf365f04b03145da09ea5411c14a0a4f2ce
habyaad <adeomotoshan@gmail.com>
2024-05-22 16:30:24 +0100

    feat: provide endpoint to fund wallet & fetch txns

--- src/transaction/transaction.controller.ts
+++ src/transaction/transaction.controller.ts
@@ -1,11 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { FundWalletDto } from './dto/fund-wallet.dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/common/entities/user.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Transaction } from './entities/transaction.entity';
 
 @Controller('transaction')
+@ApiTags('Transaction')
+@ApiBearerAuth()
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
+
+  @Post('/fundWallet')
+  @UseGuards(JwtAuthGuard)
+  async create(
+    @GetUser() user: User,
+    @Body(ValidationPipe) fundWalletDto: FundWalletDto,
+  ) {
+    return await this.transactionService.fundWallet(user, fundWalletDto.amount);
+  }
 
   // @Post()
   // create(@Body() createTransactionDto: CreateTransactionDto) {
@@ -16,6 +43,12 @@ export class TransactionController {
   // findAll() {
   //   return this.transactionService.findAll();
   // }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async fetchUserTransactions(@GetUser() user: User): Promise<Transaction[]> {
+    return await this.transactionService.fetchUserTransactions(user.id);
+  }
 
   // @Get(':id')
   // findOne(@Param('id') id: string) {
