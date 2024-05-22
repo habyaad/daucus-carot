9f07fc8739a6f074c04e44f09f013fc14309f217
habyaad <adeomotoshan@gmail.com>
2024-05-22 16:31:18 +0100

    feat: write method to fund wallet and fetch txns

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,11 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import {
+  TransactionCategory,
+  TransactionStatus,
+  TransactionType,
+} from 'src/common/enums';
+import { WalletService } from 'src/wallet/wallet.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
+import { TransactionRepository } from './transaction.repository';
+import { User } from 'src/common/entities/user.entity';
+import { StringUtils } from 'src/common/helpers/string.utils';
+import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class TransactionService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  constructor(
+    private readonly transactionRepository: TransactionRepository,
+    private readonly walletService: WalletService,
+  ) {}
+  async create(
+    createTransactionDto: CreateTransactionDto,
+  ): Promise<Transaction> {
+    const transaction: Transaction = new Transaction();
+    transaction.reference = createTransactionDto.reference;
+    transaction.reason = createTransactionDto.reason;
+    transaction.status = TransactionStatus.Success;
+    transaction.amount = createTransactionDto.amount;
+    transaction.category = createTransactionDto.category;
+    transaction.transactionType =
+      createTransactionDto.category === TransactionCategory.Funding ||
+      TransactionCategory.Receipt
+        ? TransactionType.Credit
+        : TransactionType.Debit;
+    transaction.narration = createTransactionDto.narration;
+    transaction.user = createTransactionDto.user;
+    console.log(transaction);
+    await transaction.save();
+    return transaction;
   }
 
   findAll() {
@@ -15,12 +47,35 @@ export class TransactionService {
   findOne(id: number) {
     return `This action returns a #${id} transaction`;
   }
-
+  async fetchUserTransactions(userId: string): Promise<Transaction[]> {
+    return await this.transactionRepository.find({
+      where: {
+        user: {
+          id: userId,
+        },
+      },
+    });
+  }
   update(id: number, updateTransactionDto: UpdateTransactionDto) {
     return `This action updates a #${id} transaction`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} transaction`;
+  }
+  @Transactional()
+  async fundWallet(user: User, amount: number) {
+    await this.walletService.creditWallet(user.wallet, amount);
+    const transaction: Transaction = await this.create({
+      reference: StringUtils.generateTransactionReference(),
+      amount: amount,
+      category: TransactionCategory.Funding,
+      narration: StringUtils.generateNarration(
+        TransactionCategory.Funding,
+        user.firstName,
+      ),
+      user: user,
+    });
+    return transaction;
   }
 }
