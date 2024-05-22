3df1d6773270a939ce1ee78e5bb8dfb8e841087c
habyaad <adeomotoshan@gmail.com>
2024-05-22 16:27:06 +0100

    feat: provide endpoint to fetch user wallet

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from 'src/common/entities/user.entity';
+import { Wallet } from './entities/wallet.entity';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { RolesGuard } from 'src/auth/guards/role.guard';
 
 @Controller('wallet')
+@ApiBearerAuth()
+@ApiTags('Wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
@@ -21,6 +30,11 @@ export class WalletController {
   // findOne(@Param('id') id: string) {
   //   return this.walletService.findOne(+id);
   // }
+  @Get('/me')
+  @UseGuards(JwtAuthGuard)
+  async findOne(@GetUser() user:User):Promise<Wallet>  {
+    return await this.walletService.findOneByUser(user.id);
+  }
 
   // @Patch(':id')
   // update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
