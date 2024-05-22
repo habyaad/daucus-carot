c7a39c55a9d44939b2e25b932a8271dbdd767aee
habyaad <adeomotoshan@gmail.com>
2024-05-22 16:30:42 +0100

    feat: import wallet module

--- src/transaction/transaction.module.ts
+++ src/transaction/transaction.module.ts
@@ -4,10 +4,12 @@ import { TransactionController } from './transaction.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Transaction } from './entities/transaction.entity';
 import { TransactionRepository } from './transaction.repository';
+import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Transaction])],
+  imports:[TypeOrmModule.forFeature([Transaction]), WalletModule],
   controllers: [TransactionController],
   providers: [TransactionService, TransactionRepository],
+  exports:[TransactionService]
 })
 export class TransactionModule {}
